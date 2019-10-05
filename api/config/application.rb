c74618387d9f5b3b62052c0773e2fadcafeb4e24
Daniel Klasson <dan.g.klasson@gmail.com>
2019-10-05 14:28:13 +0800

    added jwt

--- api/config/application.rb
+++ api/config/application.rb
@@ -33,5 +33,8 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Include lib folder
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
